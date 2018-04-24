e21ee34097064f24f61c76a79e2b9185b1cc14a0
Robert Grizzard <robertgrizzard@roberts-mbp.home>
2018-04-23 23:21:10 -0400

    Made some progress on the initial screen, icons and buttons with stub actions.

--- MPQuizzer-/MPQuizzer-/ViewController.swift
+++ MPQuizzer-/MPQuizzer-/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleView: UIImageView!
+    @IBOutlet weak var singlePlayerIV: UIImageView!
+    @IBOutlet weak var multiPlayerIV: UIImageView!
+    
+    @IBOutlet weak var singlePlayerButton: UIButton!
+    @IBOutlet weak var multiPlayerButton: UIButton!
+    
+    var singleOrMulti: String!
+    let defaultButtonTextColor = UIButton(type: UIButtonType.system).titleColor(for: .normal)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let titleImage = UIImage(named: "Title.png")
+        titleView.image = titleImage
+        
+        let singlePlayerImage = UIImage(named: "singlePlayer.png")
+        singlePlayerIV.image = singlePlayerImage
+        
+        let multiPlayerImage = UIImage(named: "multiPlayer.png")
+        multiPlayerIV.image = multiPlayerImage
+        
+        singlePlayerButton.layer.cornerRadius = 0.25 * singlePlayerButton.frame.height
+        multiPlayerButton.layer.cornerRadius = 0.25 * multiPlayerButton.frame.height
+        singlePlayerButton.clipsToBounds = true
+        multiPlayerButton.clipsToBounds = true
+        
+        singleOrMulti = "single"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func singlePlayerButtonTapped(_ sender: UIButton) {
+        
+        //print("Single player tapped")
+        singleOrMulti = "single"
+        singlePlayerButton.backgroundColor = defaultButtonTextColor
+        singlePlayerButton.setTitleColor(UIColor.white, for: .normal)
+        multiPlayerButton.backgroundColor = UIColor.white
+        multiPlayerButton.setTitleColor(defaultButtonTextColor, for: .normal)
+    }
+    
+    @IBAction func multiPlayerButtonTapped(_ sender: UIButton) {
+        
+        //print("Multi player tapped")
+        singleOrMulti = "multi"
+        multiPlayerButton.backgroundColor = defaultButtonTextColor
+        multiPlayerButton.setTitleColor(UIColor.white, for: .normal)
+        singlePlayerButton.backgroundColor = UIColor.white
+        singlePlayerButton.setTitleColor(defaultButtonTextColor, for: .normal)
+    }
+    
+    
+    @IBAction func connectBarButtonTapped(_ sender: UIBarButtonItem) {
+        let temporaryAlert = UIAlertController(title: "ALERT", message: "Implement multiPeer Connectivity", preferredStyle: .alert)
+        temporaryAlert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        self.present(temporaryAlert, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func startQuizButtonTapped(_ sender: UIButton) {
+        switch singleOrMulti {
+        case "single":
+            print("starting with single player")
+        case "multi":
+            print("starting with multiple players")
+        default:
+            print("unexpected default in startQuizButtonTapped, check value of \"singleOrMulti\"")
+        }
+        
+        let temporaryAlert = UIAlertController(title: "ALERT", message: "Implement segue to next screen", preferredStyle: .alert)
+        temporaryAlert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        self.present(temporaryAlert, animated: true, completion: nil)
+        
+    }
 }
 
