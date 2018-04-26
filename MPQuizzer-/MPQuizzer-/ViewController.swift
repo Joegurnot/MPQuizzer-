ad8a9380483bfe1140be83b12d1a9fa0d607af17
Joseph Conteh <contehji@vcu.edu>
2018-04-26 09:15:24 -0400

    Muliti-peer necessary methods and delegate handling

--- MPQuizzer-/MPQuizzer-/ViewController.swift
+++ MPQuizzer-/MPQuizzer-/ViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
+    
+    var session: MCSession!
+    var peerID: MCPeerID!
+    
+    var browser: MCBrowserViewController!
+    var assistant: MCAdvertiserAssistant!
+    
+    
+    
+    
 
     @IBOutlet weak var titleView: UIImageView!
     @IBOutlet weak var singlePlayerIV: UIImageView!
@@ -23,6 +34,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpConnectivity()
         
         let titleImage = UIImage(named: "Title.png")
         titleView.image = titleImage
@@ -41,7 +54,24 @@ class ViewController: UIViewController {
         singleOrMulti = "single"
         
     }
+    
+    func setUpConnectivity() {
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID)
+        self.browser = MCBrowserViewController(serviceType: "MPQuizzer", session: session)
+        self.assistant = MCAdvertiserAssistant(serviceType: "MPQuizzer", discoveryInfo: nil, session: session)
+        
+        session.delegate = self
+        browser.delegate = self
+        
+        assistant.start()
+        present(browser, animated: true)
 
+    }
+    
+    func startHosting(action: UIAlertAction){
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -91,5 +121,40 @@ class ViewController: UIViewController {
         self.present(temporaryAlert, animated: true, completion: nil)
         
     }
-}
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }}
 
